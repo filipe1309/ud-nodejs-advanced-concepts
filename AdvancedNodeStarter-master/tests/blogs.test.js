dbcf6ad409bc5999269d11bc67223fdc2c91c28c
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-06-14 19:19:18 -0300

    refactor(section-5): add describe jest block to simplify tests

--- AdvancedNodeStarter-master/tests/blogs.test.js
+++ AdvancedNodeStarter-master/tests/blogs.test.js
@@ -11,9 +11,14 @@ afterEach(async () => {
     await page.close()
 });
 
-test('When logged in, can see blog create form', async () => {
-    await page.login();
-    await page.click('a[href="/blogs/new"]');
-    const label = await page.getContentsOf('form label');
-    expect(label).toEqual('Blog Title');
+describe('When logged in', async () => {
+    beforeEach(async () => {
+        await page.login();
+        await page.click('a[href="/blogs/new"]');
+    });
+
+    test('can see blog create form', async () => {
+        const label = await page.getContentsOf('form label');
+        expect(label).toEqual('Blog Title');
+    });
 });
