82c157966690421310b5ba79e46a55c3725b1921
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-06-24 17:29:09 -0300

    feat(section-07): configure aws sdk

--- AdvancedNodeStarter/routes/uploadRoutes.js
+++ AdvancedNodeStarter/routes/uploadRoutes.js
@@ -1,3 +1,14 @@
+const aws = require('aws-sdk');
+const keys = require('../config/keys');
+
+const s3 = new aws.S3({
+  credentials: {
+    accessKeyId: keys.accessKeyId,
+    secretAccessKey: keys.secretAccessKey,
+  },
+  region: 'us-east-1',
+});
+
 module.exports = app => {
   app.get('/api/upload', (req, res) => {
     res.send({ hi: 'there' });
